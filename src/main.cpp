8f0c34d3bc1f38a4265a5ae7da8ded3a83b5bdb5
JamesXiaoMo <jameswuxiaomo@gmail.com>
2025-01-04 02:49:44 +0900

    优化注释和精简代码

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,37 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <FastLED.h>
-
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+// --------------------- 以下全局参数 ---------------------
+// BLE全局参数
 #define SERVICE_UUID "66de1d0a-0dd9-44f0-8fa1-85f39668a27c"
 #define CHARACTERISTIC_UUID "6f7e38d5-3acd-42f2-99aa-adf6a410ff92"
-
-#define CONNECT_TIMEOUT 10000
-
 BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-
+// WiFi全局参数
+#define CONNECT_WIFI_TIMEOUT 10000
 bool isGetWiFiData = false;
 std::string WIFIDATA;
-
+// 电机全局参数
+int DCMOTOR_SPEED, StepperMotor_SPEED;
+// WS2812全局参数
 CRGB leds[1];
-
 WiFiServer server(7769);
+// -------------------------------------------------------
 
-void WiFiConnect()
-{
+// --------------------- 以下全局函数 ---------------------
+void WiFiConnect(){
   isGetWiFiData = false;
   std::string SSID, PWD;
   size_t delimiterPos = WIFIDATA.find('|');
-  if (delimiterPos != std::string::npos)
-  {
+  if (delimiterPos != std::string::npos){
     SSID = WIFIDATA.substr(0, delimiterPos);
     PWD = WIFIDATA.substr(delimiterPos + 1);
   }
-  else
-  {
+  else{
     Serial.println("Invalid WiFiData format!");
     return;
   }
@@ -42,26 +40,23 @@ void WiFiConnect()
   Serial.print("Password: ");
   Serial.println(PWD.c_str());
   WiFi.begin(SSID.c_str(), PWD.c_str());
-  // 开始连接 Wi-Fi
+  // 连接 Wi-Fi
   unsigned long startTime = millis();
-  while (WiFi.status() != WL_CONNECTED && (millis() - startTime) < CONNECT_TIMEOUT)
-  {
+  while (WiFi.status() != WL_CONNECTED && (millis() - startTime) < CONNECT_WIFI_TIMEOUT){
     delay(1000);
     Serial.print("Connecting to Wi-Fi");
     Serial.print(".");
   }
-
-  if (WiFi.status() == WL_CONNECTED)
-  {
+  if (WiFi.status() == WL_CONNECTED){
     Serial.println("\nConnected to Wi-Fi");
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
+    // 开启TCP服务
     server.begin();
     Serial.print("TCP server started on ");
     Serial.println("7769");
   }
-  else
-  {
+  else{
     Serial.println("\nFailed to connect to Wi-Fi");
   }
 }
@@ -69,141 +64,106 @@ void WiFiConnect()
 class MyCallbacks : public BLECharacteristicCallbacks
 {
   std::string rxValueAll;
-  void onWrite(BLECharacteristic *pCharacteristic)
-  {
+  void onWrite(BLECharacteristic *pCharacteristic){
     std::string rxValue = pCharacteristic->getValue();
-    if (rxValue.find("^") != std::string::npos)
-    {
+    if (rxValue.find("^") != std::string::npos){
       WIFIDATA.clear();
       rxValueAll.clear();
-      Serial.println("Start");
+      // Serial.println("Start");
     }
-    else if (rxValue.find("$") != std::string::npos)
-    {
+    else if (rxValue.find("$") != std::string::npos){
       WIFIDATA = rxValueAll;
       rxValueAll.clear();
       isGetWiFiData = true;
-      Serial.println("End");
+      //Serial.println("End");
     }
-    else
-    {
+    else{
       rxValueAll += rxValue;
     }
-    Serial.println("Received data via BLE:");
-    Serial.println(rxValue.c_str());
+    // Serial.println("Received data via BLE:");
+    // Serial.println(rxValue.c_str());
   }
 };
 
-class MyServerCallbacks : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    deviceConnected = true;
-    Serial.println("Device connected");
+class MyServerCallbacks : public BLEServerCallbacks{
+  void onConnect(BLEServer *pServer){
+    Serial.println("BLE Device connected");
   }
-
-  void onDisconnect(BLEServer *pServer)
-  {
-    deviceConnected = false;
-    Serial.println("Device disconnected");
+  void onDisconnect(BLEServer *pServer){
+    Serial.println("BLE Device disconnected");
   }
 };
+// -------------------------------------------------------
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
+  // 初始化WS2812
+  FastLED.addLeds<WS2812, 48, GRB>(leds, 1);
+  FastLED.setBrightness(100);
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  // 初始化BLE
   BLEDevice::init("ESP32S3_BLE");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
-
   BLEService *pService = pServer->createService(SERVICE_UUID);
-
   pCharacteristic = pService->createCharacteristic(
       CHARACTERISTIC_UUID,
       BLECharacteristic::PROPERTY_READ |
       BLECharacteristic::PROPERTY_WRITE);
-  // 设置回调
   pCharacteristic->setCallbacks(new MyCallbacks());
-
-  // 添加描述
   pCharacteristic->addDescriptor(new BLE2902());
-
-  // 开启服务
   pService->start();
-
-  // 开启广播
   pServer->getAdvertising()->start();
   Serial.println("BLE is ready to receive data");
-
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->start();
-
-  FastLED.addLeds<WS2812, 48, GRB>(leds, 1);
-  FastLED.setBrightness(100);
-
-  leds[0] = CRGB::Red;
-  FastLED.show();
 }
 
-void loop()
-{
-  if (isGetWiFiData)
-  {
+void loop(){
+  if (isGetWiFiData){
     WiFiConnect();
   }
-  if (WiFi.status() == WL_CONNECTED)
-  {
-
-    while (WiFi.status() == WL_CONNECTED)
-    {
-      WiFiClient client = server.available();
-      
-      if (client)
-      {
-        client.setNoDelay(true);
-        Serial.println("New client connected");
-        while (client.connected())
-        {
-          if (client.available())
-          {
-            String data = client.readStringUntil('\n');
-            if (data == "Connect")
-            {
-              client.println("ConnectOK");
-              leds[0] = CRGB::Green;
-              FastLED.show();
-              // Serial.println(">>> ConnectOK");
+  while (WiFi.status() == WL_CONNECTED){
+    WiFiClient client = server.available();
+    if (client){
+      client.setNoDelay(true);
+      Serial.println("New client connected");
+      while (client.connected()){
+        if (client.available()){
+          String data = client.readStringUntil('\n');
+          if (data == "Connect"){
+            client.println("ConnectOK");
+            leds[0] = CRGB::Green;
+            FastLED.show();
+          }else if (data == "RSSI"){
+            client.println("+" + String(WiFi.RSSI()));
+          }else if (data.charAt(0) == '@'){
+            size_t numOfPos = 2;
+            int pos[numOfPos] = {-1, -1};
+            for (int i = 0; i < data.length(); i++){
+              if (data.charAt(i) == '|'){
+                for (int j = 0; j < numOfPos; j++){
+                  if (pos[j] == -1){
+                    pos[j] = i;
+                    break;
+                  }
+                }
+              }
             }
-            else if (data == "RSSI")
-            {
-              client.println("+" + String(WiFi.RSSI()));
-            }
-            else if (data.charAt(0) == '@')
-            {
-              Serial.println(data);
-            }
+            DCMOTOR_SPEED = data.substring(1, pos[0]).toInt();
+            StepperMotor_SPEED = data.substring(pos[0] + 1, pos[1]).toInt();
+            Serial.println("DCMOTOR_SPEED: " + String(DCMOTOR_SPEED));
+            Serial.println("StepperMotor_SPEED: " + String(StepperMotor_SPEED));
           }
         }
       }
-      else
-      {
-        if (WiFi.status() == WL_CONNECTED)
-        {
-          leds[0] = CRGB::Yellow;
-          FastLED.show();
-        }
-        else
-        {
-          leds[0] = CRGB::Red;
-          FastLED.show();
-        }
-      }
+    }else{
+      leds[0] = CRGB::Yellow;
+      FastLED.show();
     }
   }
-  else
-  {
-    Serial.println("Waiting for WiFi connection...");
-    delay(1000);
-  }
+  Serial.println("Waiting for WiFi connection...");
+  delay(1000);
 }
